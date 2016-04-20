0378c219bc4313ca184b66403e735d36bf5dc2dc
jforge <github@jforge.eu>
2016-04-20 07:08:05 +0200

    Added support for BrickletLoadCell

--- brick-mqtt-proxy.py
+++ brick-mqtt-proxy.py
@@ -71,7 +71,7 @@ from tinkerforge.bricklet_lcd_20x4 import BrickletLCD20x4
 # FIXME: LED Strip Bricklet not handled yet
 from tinkerforge.bricklet_line import BrickletLine
 from tinkerforge.bricklet_linear_poti import BrickletLinearPoti
-# FIXME: Load Cell Bricklet not handled yet
+from tinkerforge.bricklet_load_cell import BrickletLoadCell
 from tinkerforge.bricklet_moisture import BrickletMoisture
 from tinkerforge.bricklet_motion_detector import BrickletMotionDetector
 # FIXME: Multi Touch Bricklet not handled yet
@@ -746,7 +746,18 @@ class BrickletLinearPotiProxy(DeviceProxy):
     TOPIC_PREFIX = 'bricklet/linear_poti'
     GETTER_SPECS = [('get_position', 'position', 'position')]
 
-# FIXME: Load Cell Bricklet not handled yet
+class BrickletLoadCell(DeviceProxy):
+    DEVICE_CLASS = BrickletLoadCell
+    TOPIC_PREFIX = 'bricklet/load_cell'
+    GETTER_SPECS = [('get_weight', 'weight', 'weight'),
+                    ('is_led_on', 'led_on', 'on'),
+                    ('get_moving_average', 'moving_average', 'average'),
+                    ('get_configuration', 'configuration', None)]
+    SETTER_SPECS = [('set_moving_average', 'moving_average/set', ['average']),
+                    ('led_on', 'led_on/set', []),
+                    ('led_off', 'led_off/set', []),
+                    ('tare', 'tare/set', []),
+                    ('set_configuration', 'configuration/set', ['rate', 'gain'])]
 
 class BrickletMoistureProxy(DeviceProxy):
     DEVICE_CLASS = BrickletMoisture
